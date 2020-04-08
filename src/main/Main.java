a0a3c3df6036872579495206c5e1c18bcc4b9f51
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-04-08 16:20:12 +0530

    create a greet method

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,4 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+    }
 }
